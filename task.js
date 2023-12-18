2e23938eddf9e64bf2bcf0890efab493e18dcfba
Danil Krutikov <kdanil.i@yandex.ru>
2023-12-18 19:42:17 +0700

    Second commit

--- task.js
+++ task.js
@@ -4,17 +4,12 @@ button.addEventListener("click", (e) => {
   e.preventDefault()
   let input = document.getElementById("task__input")
   let task = document.getElementById("tasks__list")
-  let div1 = document.createElement("DIV")
-  div1.className = "task"
-  task.appendChild(div1)
-  div1.innerHTML = "<a href='#' class='task__remove'>&times;</a>"
-  let div2 = document.createElement("DIV")
-  div2.textContent = input.value
-  div2.className = "task__title"
-  div1.insertBefore(div2, div1.firstElementChild)
-  const dels = document.getElementsByClassName("task__remove")
+  if (input.value.trim() != "") {
+    task.insertAdjacentHTML("beforeEnd", `<div class="task"><div class="task__title">${input.value}</div><a href="#" class="task__remove">&times;</a></div>`)
+  input.value = ""
+  }
   let taskChildren = task.children
-  for (i=0; i<taskChildren.length; i++) {
+  for (let i=0; i<taskChildren.length; i++) {
     let a = taskChildren[i].children[1]
     a.addEventListener('click', (e) => {
       e.target.parentElement.remove()
